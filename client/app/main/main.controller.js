53ddf637025d34975a21a273c7ab3127824aa470
Matt Everson <meverson@tenable.com>
2015-08-26 00:42:05 -0500

    Clean up home page a bit

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,11 +1,26 @@
 'use strict';
 
 angular.module('workspaceApp')
-  .controller('MainCtrl', function ($scope, PollsModel) {
+  .controller('MainCtrl', function ($scope, PollsModel, Auth) {
     $scope.polls = [];
+    $scope.loggedIn = Auth.isLoggedIn();
+
+    function getBarData(poll) {
+        var labels = poll.options.map(function(opt) { return opt.name; });
+        var data = poll.options.map(function(opt) { return opt.votes; });
+        return {
+          labels: labels,
+          datasets: [{
+            data: data
+          }]
+        };
+    };
 
     PollsModel.all()
     .then(function(result) {
-        $scope.polls = result.data;
+        $scope.polls = result.data.map(function(poll) {
+          poll.barData = getBarData(poll);
+          return poll;
+        });
     })
   });
